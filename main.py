08cf7e9ef1e82cd0d19cee6d73ae6cf2d435eafc
Artyom Lezgyan <33369719+Paleontolog@users.noreply.github.com>
2020-03-11 02:44:25 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,8 @@
 import os
 import argparse
 import re
+import json
 
-from pyspark.sql.context import SQLContext
-
-from database.s3_utils import *
-from metrics.calculate_metrics import *
 
 with open('./config.json') as json_data_file:
     app_config = json.load(json_data_file)
@@ -110,6 +107,11 @@ if __name__ == "__main__":
     from pyspark import RDD
     from pyspark import SparkConf, SparkContext
     from pyspark.streaming.kinesis import KinesisUtils, InitialPositionInStream
+    from pyspark.sql.context import SQLContext
+
+    from database.s3_utils import *
+    from metrics.calculate_metrics import *
+    
 
     schema = st.StructType.fromJson(app_config["all_data_scheme"])
 
